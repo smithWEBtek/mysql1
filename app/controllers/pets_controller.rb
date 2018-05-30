4802cf6488e0b83dfdf468afd6904500ea0586b1
smithwebtek <brad@smithwebtek.com>
2018-05-30 18:17:35 -0400

    Add bootstrap, add pet randomizer

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+
+	def randomize
+		Pet.all.each do |p| 
+			custid = (0..Customer.all.count).to_a.sample
+			p.customer_id = custid
+			p.save
+		end
+		@customers = Customer.all
+		render 'customers/index'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :pets
   resources :customers
  
+	get '/randomize_pets', to: 'pets#randomize'
+
+	root 'customers#index'
 	
 end
